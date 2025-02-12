11c7245dced8c4afb93ea0186837756a5d351765
zuzusann <zuzusan65@gmail.com>
2025-02-12 16:46:20 +0630

    [ADD] form validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +13,9 @@ export class AppComponent implements OnInit{
 
   ngOnInit(){
       this.reativeForm = new FormGroup({
-        firstname: new FormControl(null),
-        lastname: new FormControl(null),
-        email: new FormControl(null),
+        firstname: new FormControl(null, Validators.required),
+        lastname: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
         username: new FormControl(null),
         dob: new FormControl(null),
         gender: new FormControl('male'),
