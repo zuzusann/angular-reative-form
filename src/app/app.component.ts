571d923a845a49d88275d7f86fba4ebef2d2b512
zuzusann <zuzusan65@gmail.com>
2025-02-12 16:00:34 +0630

    [ADD]  Initialize reactive form with FormGroup and FormControl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-reative-form';
+
+  reativeForm: FormGroup;
+
+  ngOnInit(){
+      this.reativeForm = new FormGroup({
+        firstname: new FormControl(null),
+        lastname: new FormControl(null),
+        email: new FormControl(null),
+        username: new FormControl(null),
+        dob: new FormControl(null),
+        gender: new FormControl('male'),
+        street: new FormControl(null),
+        country: new FormControl('Japan'),
+        city: new FormControl(null),
+        region: new FormControl(null),
+        postal: new FormControl(null)
+      });
+  }
+  
+  OnFormSubmitted(){
+    console.log(this.reativeForm);
+  }
+
 }
